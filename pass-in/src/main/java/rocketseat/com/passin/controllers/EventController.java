efd2b09da96d855fb8a41052fbe01928a241396c
Vitor Duarte Passo <vitor.passo@codex.com.br>
2024-04-03 21:15:49 -0300

    Continuação da aula 2 do NWL

--- pass-in/src/main/java/rocketseat/com/passin/controllers/EventController.java
+++ pass-in/src/main/java/rocketseat/com/passin/controllers/EventController.java
@@ -1,15 +1,28 @@
 package rocketseat.com.passin.controllers;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+import rocketseat.com.passin.dto.event.EventIdDTO;
+import rocketseat.com.passin.dto.event.EventRequestDTO;
+import rocketseat.com.passin.dto.event.EventResponseDTO;
+import rocketseat.com.passin.services.EventService;
 
 @RestController
 @RequestMapping("/events")
+@RequiredArgsConstructor
 public class EventController {
-    @GetMapping
-    public ResponseEntity<String> getTestEvent() {
-        return ResponseEntity.ok("Sucess, endpoint 'Event' on in the server...");
+    private final EventService eventService;
+    @GetMapping("/{id}")
+    public ResponseEntity<EventResponseDTO> getEvent(@PathVariable String id) {
+        EventResponseDTO event = this.eventService.getEventDetail(id);
+        return ResponseEntity.ok(event);
+    }
+    @PostMapping
+    public ResponseEntity<EventIdDTO> createEvent(@RequestBody EventRequestDTO body, UriComponentsBuilder uriComponentsBuilder) {
+        EventIdDTO eventIdDTO = this.eventService.createEvent(body);
+        var uri = uriComponentsBuilder.path("/event/{id}").buildAndExpand(eventIdDTO.eventId()).toUri();
+        return ResponseEntity.created(uri).body(eventIdDTO);
     }
 }
--- pass-in/src/main/java/rocketseat/com/passin/repository/AttendeeRepository.java
+++ pass-in/src/main/java/rocketseat/com/passin/repository/AttendeeRepository.java
@@ -3,5 +3,8 @@ package rocketseat.com.passin.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import rocketseat.com.passin.domain.attendee.Attendee;
 
+import java.util.List;
+
 public interface AttendeeRepository extends JpaRepository<Attendee, String> {
+    public List<Attendee> findByEventId(String id);
 }
